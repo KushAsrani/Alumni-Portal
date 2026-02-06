2603c02535a5ccfe843f67cef2e7666fe1e56e66
KushAsrani <kushasrani@gmail.com>
2026-02-06 17:35:21 +0530

    Configure for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
